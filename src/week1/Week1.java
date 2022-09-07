ce7b25cd835413d473381b87d4e30e6567fc1fe7
f-guedes <fernando.hpg2@gmail.com>
2022-09-06 20:12:04 -0400

    first commit

---
+++ src/week1/Week1.java
@@ -0,0 +1,15 @@
+package week1;
+
+public class Week1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int aHeight = 45;
+		int bHeight = aHeight;
+		
+		String aName = "Mary";
+
+
+	}
+
+}
